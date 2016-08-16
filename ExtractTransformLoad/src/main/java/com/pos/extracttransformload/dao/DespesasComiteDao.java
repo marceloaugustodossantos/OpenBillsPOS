bed79f819bc86cfbb7dabdb246abb4375fc50a4c
Kaique Cândido <Kaique Cndido>
2016-08-16 15:16:56 -0300

    implementação DAOs

---
+++ ExtractTransformLoad/src/main/java/com/pos/extracttransformload/dao/DespesasComiteDao.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pos.extracttransformload.dao;
+
+import com.pos.extracttransformload.entities.DespesaComite;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author kaiqu
+ */
+@Stateless
+public class DespesasComiteDao {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public boolean addDespesaComite(DespesaComite dc) {
+        em.persist(dc);
+        return true;
+    }
+
+    public boolean removeDespesaComite(DespesaComite dc) {
+        em.remove(em.merge(dc));
+        return true;
+    }
+
+    public boolean refreshDespesaComite(DespesaComite dc) {
+        em.merge(dc);
+        return true;
+    }
+
+    public DespesaComite searchDespesaComite(Class<DespesaComite> entidade, Object key) {
+        return em.find(entidade, key);
+    }
+
+    public List<DespesaComite> listarDespesaComite() {
+        Query query = em.createQuery("SELECT dc FROM DespesaComite dc ORDER BY dc.dsTitulo", DespesaComite.class);
+        List<DespesaComite> despesaComites = query.getResultList();
+        if (despesaComites.size() > 0) {
+            return despesaComites;
+        }
+        return null;
+    }
+}
---
+++ ExtractTransformLoad/src/main/java/com/pos/extracttransformload/dao/ReceitasCandidatoDao.java
@@ -0,0 +1,52 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.pos.extracttransformload.dao;
+
+import com.pos.extracttransformload.entities.ReceitaCandidato;
+import java.util.List;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+
+/**
+ *
+ * @author kaiqu
+ */
+@Stateless
+public class ReceitasCandidatoDao {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public boolean addReceitaCandidato(ReceitaCandidato rc) {
+        em.persist(rc);
+        return true;
+    }
+
+    public boolean removeReceitaCandidato(ReceitaCandidato rc) {
+        em.remove(em.merge(rc));
+        return true;
+    }
+
+    public boolean refreshReceitaCandidato(ReceitaCandidato rc) {
+        em.merge(rc);
+        return true;
+    }
+
+    public ReceitaCandidato searchReceitaCandidato(Class<ReceitaCandidato> entidade, Object key) {
+        return em.find(entidade, key);
+    }
+
+    public List<ReceitaCandidato> listarReceitaCandidato() {
+        Query query = em.createQuery("SELECT rc FROM ReceitaCandidato rc ORDER BY rc.cdUfDoador", ReceitaCandidato.class);
+        List<ReceitaCandidato> receitaCandidatos = query.getResultList();
+        if (receitaCandidatos.size() > 0) {
+            return receitaCandidatos;
+        }
+        return null;
+    }
+}
